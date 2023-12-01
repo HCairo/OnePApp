7a9e1c191241b770e80ac869011e908ddf70ebec
Duraygue <hugo.cairoh@gmail.com>
2023-12-01 09:58:21 +0100

    Adding assets/img for the logo

---
+++ home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header id="menu">
+        <a href="">
+            <img src="" alt="">
+        </a>
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">About us</a>
+            <a href="#">Services</a>
+            <a href="#">Contact</a>
+        </nav>
+    </header>
+</body>
+</html>
