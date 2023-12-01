331481dabe33a2c82e00a81e12dd7037583a35b8
Duraygue <hugo.cairoh@gmail.com>
2023-12-01 10:48:30 +0100

    Several mod. on css - Added footer and sections

--- home.php
+++ home.php
@@ -4,19 +4,67 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MyApp</title>
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="shortcut icon" href="assets/img/logo.ico">
+    <link rel="apple-touch-icon" href="assets/img/logo.ico">
 </head>
 <body>
+    <!-- HEADER -->
     <header id="menu">
         <a href="#">
             <img src="assets/img/logo.webp" alt="logo">
         </a>
+        <!-- NAV -->
         <nav>
             <a href="#">Home</a>
-            <a href="#">About us</a>
-            <a href="#">Services</a>
-            <a href="#">Contact</a>
+            <a href="#aboutus">About us</a>
+            <a href="#services">Services</a>
+            <a href="#contact">Contact</a>
         </nav>
+        <!-- END NAV -->
     </header>
+    <!-- END HEADER -->
+
+    <!-- MAIN -->
+    <main>
+        <!-- ABOUT US -->
+        <section id="aboutus">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe ipsam illum inventore quis exercitationem neque earum aspernatur sed excepturi fuga. Voluptatibus, distinctio dolor! Aut suscipit in deleniti, alias nulla quod perferendis nostrum. Enim expedita quis iure dolores quibusdam magni voluptas ab iste? Quaerat ab nam, provident modi nostrum doloribus nesciunt voluptatum perferendis et suscipit earum non quasi exercitationem excepturi ea cum, sunt dolor facilis? Ipsam reprehenderit nesciunt esse dicta voluptatem recusandae provident ipsa repellendus earum et ab tenetur dolor in perferendis, possimus asperiores dignissimos fuga accusamus! Dolore amet sit deserunt debitis placeat? Quia autem porro voluptatibus natus dicta quos nulla nihil similique modi, aliquam, esse sapiente unde. Quia nihil inventore ipsum aspernatur quisquam officia laudantium doloremque dolorum eveniet voluptates eum, accusantium ducimus recusandae neque numquam obcaecati suscipit fuga veniam. Quia ex modi nesciunt. Eum suscipit maxime, nulla illo obcaecati error vero laudantium delectus eaque modi impedit illum explicabo et consequatur.</section>
+        <!-- END ABOUT US -->
+
+        <!-- SERVICES -->
+        <section id="services">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe ipsam illum inventore quis exercitationem neque earum aspernatur sed excepturi fuga. Voluptatibus, distinctio dolor! Aut suscipit in deleniti, alias nulla quod perferendis nostrum. Enim expedita quis iure dolores quibusdam magni voluptas ab iste? Quaerat ab nam, provident modi nostrum doloribus nesciunt voluptatum perferendis et suscipit earum non quasi exercitationem excepturi ea cum, sunt dolor facilis? Ipsam reprehenderit nesciunt esse dicta voluptatem recusandae provident ipsa repellendus earum et ab tenetur dolor in perferendis, possimus asperiores dignissimos fuga accusamus! Dolore amet sit deserunt debitis placeat? Quia autem porro voluptatibus natus dicta quos nulla nihil similique modi, aliquam, esse sapiente unde. Quia nihil inventore ipsum aspernatur quisquam officia laudantium doloremque dolorum eveniet voluptates eum, accusantium ducimus recusandae neque numquam obcaecati suscipit fuga veniam. Quia ex modi nesciunt. Eum suscipit maxime, nulla illo obcaecati error vero laudantium delectus eaque modi impedit illum explicabo et consequatur.</section>
+        <!-- END SERVICES -->
+
+        <!-- CONTACT -->
+        <section id="contact">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe ipsam illum inventore quis exercitationem neque earum aspernatur sed excepturi fuga. Voluptatibus, distinctio dolor! Aut suscipit in deleniti, alias nulla quod perferendis nostrum. Enim expedita quis iure dolores quibusdam magni voluptas ab iste? Quaerat ab nam, provident modi nostrum doloribus nesciunt voluptatum perferendis et suscipit earum non quasi exercitationem excepturi ea cum, sunt dolor facilis? Ipsam reprehenderit nesciunt esse dicta voluptatem recusandae provident ipsa repellendus earum et ab tenetur dolor in perferendis, possimus asperiores dignissimos fuga accusamus! Dolore amet sit deserunt debitis placeat? Quia autem porro voluptatibus natus dicta quos nulla nihil similique modi, aliquam, esse sapiente unde. Quia nihil inventore ipsum aspernatur quisquam officia laudantium doloremque dolorum eveniet voluptates eum, accusantium ducimus recusandae neque numquam obcaecati suscipit fuga veniam. Quia ex modi nesciunt. Eum suscipit maxime, nulla illo obcaecati error vero laudantium delectus eaque modi impedit illum explicabo et consequatur.</section>
+        <!-- END CONTACT -->
+    </main>
+    <!-- END MAIN -->
+
+    <!-- FOOTER -->
+    <footer id="footer">
+        <div>
+            <a href="#">MyApp</a>
+            <p>Location : ...</p>
+            <p>Phone : ...</p>
+            <p>JSP : ...</p>
+        </div>
+        <nav>
+            <a href="#">Iste.</a>
+            <a href="#">Lorem.</a>
+            <a href="#">Rem.</a>
+        </nav>
+        <nav>
+            <a href="#">Lorem.</a>
+            <a href="#">Officiis?</a>
+            <a href="#">Ab.</a>
+        </nav>
+        <nav>
+            <a href="#">Lorem.</a>
+            <a href="#">Veritatis.</a>
+            <a href="#">Enim?</a>
+        </nav>
+    </footer>
+    <!-- END FOOTER -->
 </body>
 </html>
